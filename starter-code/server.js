7f5f8dc45e578fb7a9a874530b308581bf4d7e58
Kayla Asay <kdasay16@gmail.com>
2017-03-27 11:49:58 -0700

    all todos

--- starter-code/server.js
+++ starter-code/server.js
@@ -6,7 +6,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
 const app = express();
-const conString = '';// TODO: Don't forget to set your own conString
+const conString = 'postgres://postgres:potatobabe@localhost:5432/kilovolt'
 const client = new pg.Client(conString);
 client.connect();
 client.on('error', function(error) {
@@ -30,11 +30,9 @@ app.get('/new', function(request, response) {
 // Routes for making API calls to enact CRUD operations on our database
 
 app.get('/articles', function(request, response) {
-  // REVIEW: This query will join the data together from our tables
-  // TODO: Write a SQL query which joins all data from articles and authors tables on the author_id value of each
-  client.query(`SELECT
-                (join somehow)
-                ON (some other crap here)`)
+  client.query(`SELECT * FROM articles
+                JOIN authors
+                ON articles.author_id = authors.author_id;`)
   .then(function(result) {
     response.send(result.rows);
   })
@@ -45,17 +43,26 @@ app.get('/articles', function(request, response) {
 
 app.post('/articles', function(request, response) {
   client.query(
-  // TODO: Write a SQL query to insert a new ***author***, ON CONFLICT DO NOTHING
-  // TODO: Add author and "authorUrl" as data for the SQL query to interpolate
-    'Thing1',
-    [Thing2]
+    `INSERT INTO authors(author, "authorUrl")
+    VALUES($1, $2) ON CONFLICT DO NOTHING;`,
+    [
+      request.body.author,
+      request.body.authorUrl
+    ]
   )
   .then(function() {
-    // TODO: Write a SQL query to insert a new ***article***, using a sub-query to retrieve the author_id from the authors table
-    // TODO: Add the required values from the request as data for the SQL query to interpolate
     client.query(
-      `Thing1`,
-      [Thing2]
+      `INSERT INTO articles(author_id, title, category, "publishedOn", body)
+      SELECT author_id, $1, $2, $3, $4
+      FROM authors
+      WHERE author=$5;`,
+    [
+      request.body.title,
+      request.body.category,
+      request.body.publishedOn,
+      request.body.body,
+      request.body.author
+    ]
     )
   })
   .then(function() {
@@ -67,18 +74,30 @@ app.post('/articles', function(request, response) {
 });
 
 app.put('/articles/:id', function(request, response) {
+  console.log('You made it', request.body.author, request.body.authorUrl, request.params.id);
   client.query(
-  // TODO: Write a SQL query to update an ***author*** record
-  // TODO: Add the required values from the request as data for the SQL query to interpolate
-    `Thing1`,
-    [Thing2]
+    `UPDATE authors
+    SET author=$1, "authorUrl"=$2
+    WHERE author_id=$3;`,
+    [
+      request.body.author,
+      request.body.authorUrl,
+      request.params.id
+    ]
   )
   .then(function() {
-    // TODO: Write a SQL query to update an **article*** record
-    // TODO: Add the required values from the request as data for the SQL query to interpolate
+    console.log('help', request.body.title, request.body.category, request.body.publishedOn, request.body.body, request.params.id);
     client.query(
-      `Thing1`,
-      [Thing2]
+      `UPDATE articles
+      SET title=$1, category=$2, "publishedOn"=$3, body=$4
+      WHERE article_id=$5;`,
+      [
+        request.body.title,
+        request.body.category,
+        request.body.publishedOn,
+        request.body.body,
+        request.params.id
+      ]
     )
   })
   .then(function() {
