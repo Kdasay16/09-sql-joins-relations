057c4dc27fe6d4fe086febdc953a87d7d30e5162
Kayla Asay <kdasay16@gmail.com>
2017-03-27 11:53:20 -0700

    weekend code

--- starter-code/server.js
+++ starter-code/server.js
@@ -6,7 +6,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
 const app = express();
-const conString = 'postgres://postgres:potatobabe@localhost:5432/kilovolt'
+const conString = 'postgres://michaelltreat:test@localhost:5432/kilovolt';// TODO: Don't forget to set your own conString
 const client = new pg.Client(conString);
 client.connect();
 client.on('error', function(error) {
@@ -30,6 +30,8 @@ app.get('/new', function(request, response) {
 // Routes for making API calls to enact CRUD operations on our database
 
 app.get('/articles', function(request, response) {
+  // REVIEW: This query will join the data together from our tables
+  // TODO: Write a SQL query which joins all data from articles and authors tables on the author_id value of each
   client.query(`SELECT * FROM articles
                 JOIN authors
                 ON articles.author_id = authors.author_id;`)
@@ -42,27 +44,25 @@ app.get('/articles', function(request, response) {
 });
 
 app.post('/articles', function(request, response) {
-  client.query(
-    `INSERT INTO authors(author, "authorUrl")
+  // TODO: Write a SQL query to insert a new ***author***, ON CONFLICT DO NOTHING
+  // TODO: Add author and "authorUrl" as data for the SQL query to interpolate
+  client.query(`
+    INSERT INTO authors(author, "authorUrl")
     VALUES($1, $2) ON CONFLICT DO NOTHING;`,
     [
       request.body.author,
-      request.body.authorUrl
+      request.body.authorUrl,
     ]
   )
   .then(function() {
-    client.query(
-      `INSERT INTO articles(author_id, title, category, "publishedOn", body)
+    // TODO: Write a SQL query to insert a new ***article***, using a sub-query to retrieve the author_id from the authors table
+    // TODO: Add the required values from the request as data for the SQL query to interpolate
+    client.query(`
+      INSERT INTO articles(author_id, title, category, "publishedOn", body)
       SELECT author_id, $1, $2, $3, $4
       FROM authors
       WHERE author=$5;`,
-    [
-      request.body.title,
-      request.body.category,
-      request.body.publishedOn,
-      request.body.body,
-      request.body.author
-    ]
+      [request.body.title, request.body.category, request.body.publishedOn, request.body.body, request.body.author]
     )
   })
   .then(function() {
@@ -74,30 +74,22 @@ app.post('/articles', function(request, response) {
 });
 
 app.put('/articles/:id', function(request, response) {
-  console.log('You made it', request.body.author, request.body.authorUrl, request.params.id);
+  // TODO: Write a SQL query to update an ***author*** record
+  // TODO: Add the required values from the request as data for the SQL query to interpolate
   client.query(
     `UPDATE authors
-    SET author=$1, "authorUrl"=$2
-    WHERE author_id=$3;`,
-    [
-      request.body.author,
-      request.body.authorUrl,
-      request.params.id
-    ]
+     SET author=$1, "authorUrl"=$2
+     WHERE author_id=$3;`,
+    [request.body.author, request.body.authorUrl, request.params.id]
   )
   .then(function() {
-    console.log('help', request.body.title, request.body.category, request.body.publishedOn, request.body.body, request.params.id);
+    // TODO: Write a SQL query to update an **article*** record
+    // TODO: Add the required values from the request as data for the SQL query to interpolate
     client.query(
       `UPDATE articles
-      SET title=$1, category=$2, "publishedOn"=$3, body=$4
-      WHERE article_id=$5;`,
-      [
-        request.body.title,
-        request.body.category,
-        request.body.publishedOn,
-        request.body.body,
-        request.params.id
-      ]
+       SET title=$1, category=$2, "publishedOn"=$3, body=$4
+       WHERE article_id=$5;`,
+      [request.body.title, request.body.category, request.body.publishedOn, request.body.body, request.params.id]
     )
   })
   .then(function() {
@@ -145,7 +137,7 @@ function loadAuthors() {
   fs.readFile('./public/data/hackerIpsum.json', function(err, fd) {
     JSON.parse(fd.toString()).forEach(function(ele) {
       client.query(
-        'INSERT INTO authors(author, "authorUrl") VALUES($1, $2) ON CONFLICT DO NOTHING',
+        'INSERT INTO authors(author, "authorUrl") VALUES($1, $2) ON CONFLICT DO NOTHING;',
         [ele.author, ele.authorUrl]
       )
     })
